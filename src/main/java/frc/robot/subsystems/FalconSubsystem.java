54ec0cf66b6ea8f31db09d8652f8812cefe0698e
Geneva-Robovikes <robovikes@geneva304.org>
2024-02-01 17:07:17 -0600

    added second falcon motor

--- src/main/java/frc/robot/subsystems/FalconSubsystem.java
+++ src/main/java/frc/robot/subsystems/FalconSubsystem.java
@@ -7,15 +7,18 @@ import edu.wpi.first.wpilibj.ADIS16448_IMU;
 
 public class FalconSubsystem extends SubsystemBase {
     TalonFX falcon;
+    TalonFX falcon2;
 
     ADIS16448_IMU gyro = new ADIS16448_IMU();
 
     public FalconSubsystem() {
         falcon = new TalonFX(3);
+        falcon2 = new TalonFX(4);
     }
 
     public void setFalcon(double speed) { //public void setFalcon
         falcon.set(speed);
+        falcon2.set(speed);
     }
 
     @Override
